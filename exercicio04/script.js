7336d74d2b07cb870eae0735ef24735a1676924c
Heitor Sales <desenvolvheitor@gmail.com>
2026-03-24 11:01:29 -0300

    Exercício 4

---
+++ exercicio04/script.js
@@ -0,0 +1,14 @@
+function tabuada() {
+    var numero = document.querySelector(".numero")
+    var resultado =  document.querySelector(".resultado")
+
+    resultado.innerText = ""
+
+    if (numero.value == "") {
+        alert("Por favor, digite um número!")
+    } else {
+        for (let i = 1; i <= 10; i++) {
+            resultado.innerText += `${numero.value} \u{00D7} ${i} = ${numero.value * i}\n`
+        }
+    }
+}
