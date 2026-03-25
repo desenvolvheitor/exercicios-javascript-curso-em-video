a8aeb0ddbcf70d3f22141c7c36d0bf6c87ba092e
Heitor Sales <desenvolvheitor@gmail.com>
2026-03-25 15:55:52 -0300

    Adição de validação (mínimo 1 número adicionado)

--- exercicio05/script.js
+++ exercicio05/script.js
@@ -8,27 +8,31 @@ function adicionar() {
     } else {
         document.querySelector(".resultado").innerText = ""
         numeros.push(numero)
-        numeros.length > 1 ? document.querySelector("#logs").value += `\n` : 
+        numeros.length > 1 ? document.querySelector("#logs").value += `\n` : ``
         document.querySelector("#logs").value += `Valor ${numero} adicionado.`
         document.querySelector("#logs").style.minHeight = `calc(1rem * ${numeros.length > 0 ? numeros.length : 1})`
     }
 }
 
 function finalizar() {
-    numeros.sort((a, b) => a - b)
+    if (numeros.length < 1) {
+        alert("Adicione valores antes de finalizar!")
+    } else {
+        numeros.sort((a, b) => a - b)
 
-    var soma = 0
-    for (let n in numeros) {
-        soma += numeros[n]
+        var soma = 0
+        for (let n in numeros) {
+            soma += numeros[n]
+        }
+
+        document.querySelector(".resultado").innerText = `Ao todo, temos ${numeros.length} números cadastrados.
+
+        O maior valor infomado foi ${numeros[numeros.length - 1]}.
+        
+        O menor valor informado foi ${numeros[0]}.
+        
+        Somando todos os valores, temos ${soma}.
+        
+        A média dos valores digitados é ${soma / numeros.length}.`
     }
-
-    document.querySelector(".resultado").innerText = `Ao todo, temos ${numeros.length} números cadastrados.
-    
-    O maior valor infomado foi ${numeros[numeros.length - 1]}.
-    
-    O menor valor informado foi ${numeros[0]}.
-    
-    Somando todos os valores, temos ${soma}.
-    
-    A média dos valores digitados é ${soma / numeros.length}.`
 }
