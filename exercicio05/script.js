ad0493652f8238e12381f38a323016c894f8a23b
Heitor Sales <desenvolvheitor@gmail.com>
2026-03-25 15:41:35 -0300

    Exercicio 5

---
+++ exercicio05/script.js
@@ -0,0 +1,34 @@
+var numeros = []
+
+function adicionar() {
+    var numero = Number(document.querySelector("#numero").value)
+    document.querySelector("#numero").value = ""
+    if (numero < 1 || numero > 100 || numeros.indexOf(numero) != -1) {
+        alert("Valor inválido ou já encontrado na lista")
+    } else {
+        document.querySelector(".resultado").innerText = ""
+        numeros.push(numero)
+        numeros.length > 1 ? document.querySelector("#logs").value += `\n` : 
+        document.querySelector("#logs").value += `Valor ${numero} adicionado.`
+        document.querySelector("#logs").style.minHeight = `calc(1rem * ${numeros.length > 0 ? numeros.length : 1})`
+    }
+}
+
+function finalizar() {
+    numeros.sort((a, b) => a - b)
+
+    var soma = 0
+    for (let n in numeros) {
+        soma += numeros[n]
+    }
+
+    document.querySelector(".resultado").innerText = `Ao todo, temos ${numeros.length} números cadastrados.
+    
+    O maior valor infomado foi ${numeros[numeros.length - 1]}.
+    
+    O menor valor informado foi ${numeros[0]}.
+    
+    Somando todos os valores, temos ${soma}.
+    
+    A média dos valores digitados é ${soma / numeros.length}.`
+}
